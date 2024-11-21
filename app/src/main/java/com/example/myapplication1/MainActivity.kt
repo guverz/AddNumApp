47ca418e47d9fc8d94ac773bef23c6729019e4da
guverz <guverz1994@mail.ru>
2024-11-21 21:11:58 +0800

    first and probably last commit

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    // TODO: 1) реализовать проверку на пустые значения в полях numA, numB
+    // TODO: 2) исправить разметку, чтобы можно было вводить только числа
+    // TODO: 3) сделать поддержку вещественных (не целых чисел)
+
+    fun onClick(v: View) {
+        val etA = findViewById<EditText>(R.id.numA)
+        val etB = findViewById<EditText>(R.id.numB)
+        val tvSum = findViewById<TextView>(R.id.sum)
+
+        val strA = etA.text.toString()
+        val strB = etB.text.toString()
+        if ((strA == "") || (strB == "")) {
+            tvSum.text = "Введено пустое значение"
+        } else {
+            val sum = strA.toFloat() + strB.toFloat()
+            val strSum = sum.toString()
+            tvSum.text = strSum
+        }
+
+
+    }
+}
